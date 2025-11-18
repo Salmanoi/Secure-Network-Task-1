346cc3a3a3be2489f19ad3e88e2ad36fcd232824
Selwan Balal <162336444+Salmanoi@users.noreply.github.com>
2025-11-18 21:21:18 +0000

    Server_application_iteration_1.py

---
+++ Server_application_iteration_1.py
@@ -0,0 +1,14 @@
+import socket, sys
+
+from datetime import datetime
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("The socket is created successfully")
+server_ip='127.0.0.1'
+server_port=1500
+
+s.connect((server_ip,server_port))
+incoming_message=s.recv(1000)
+print("Message:",incoming_message)
+print("Received at: ", str(datetime.now()))
+s.close()
