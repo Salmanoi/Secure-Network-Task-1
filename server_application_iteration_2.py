829c492e3a7d5b8168a25ea66cc30abfd72815d8
Selwan Balal <162336444+Salmanoi@users.noreply.github.com>
2025-11-28 21:08:32 +0000

    Update server_application_iteration_2.py

--- server_application_iteration_2.py
+++ server_application_iteration_2.py
@@ -1,4 +1,13 @@
 import socket
 import os
 
-from crytography.hazmat.primitives.cipher
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+
+
+
+if __name__ == "__main__":
+    server_receive_file("received_textfile.txt", "127.0.0.1", 65432)
