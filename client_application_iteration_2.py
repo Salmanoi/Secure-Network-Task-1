450cc6af34cb5255fc9519c36e24d343fadfdcaf
Selwan Balal <162336444+Salmanoi@users.noreply.github.com>
2025-11-29 22:19:21 +0000

    Update client_application_iteration_2.py

--- client_application_iteration_2.py
+++ client_application_iteration_2.py
@@ -1,7 +1,6 @@
 import socket
 import os
 
-
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
