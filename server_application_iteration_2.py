50bc15b16ec0c1e931115fa7d97bb09b8082aef8
Selwan Balal <162336444+Salmanoi@users.noreply.github.com>
2025-12-01 13:53:33 +0000

    Refactor server to handle encrypted file reception
    Enhanced the server to receive encrypted data, parse salt and IV, decrypt the payload, and save both encrypted and decrypted data to a file. Added key generation and improved error handling and logging for client connections.

--- server_application_iteration_2.py
+++ server_application_iteration_2.py
@@ -2,6 +2,7 @@ import socket
 import os
 import sys
 import threading
+from datetime import datetime
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
@@ -14,6 +15,16 @@ SERVER_IP = '127.0.0.1' # --LOOPBACK-- CHANGE THIS FOR THE CONFIGURATION OF DEMO
 SERVER_PORT = 1500 # WILL KEEP AS 1500 ALWAYS
 BUFFER_SIZE = 1024 # GETS DATA IN CHUNKS OF 1024 BYTES
 
+
+# Generate AES key from a password (for demonstration)
+def generate_key(password: str, salt: bytes) -> bytes:
+	kdf = PBKDF2HMAC(algorithm=hashes.SHA512(),
+	length=32,  # AES-256 key length
+	salt=salt,
+	iterations=100000,
+	backend=default_backend()
+	)
+	return kdf.derive(password.encode())
 class ClientThread(threading.Thread):
 	def __init__(self, ip, port, client_socket):
 		threading.Thread.__init__(self)
@@ -27,19 +38,67 @@ class ClientThread(threading.Thread):
 		full_data = b""
 
 		try:
-			# this loop is here to recieve all the data in the 1024 buffer size amount
+			# Step 1: this loop is here to recieve all the data in the 1024 buffer size amount
 			while True:
-				
+				data = self.client_socket.recv(BUFFER_SIZE)
+				if not data:
+					break
+				full_data += data
 
-# Generate AES key from a password (for demonstration)
-def generate_key(password: str, salt: bytes) -> bytes:
-	kdf = PBKDF2HMAC(algorithm=hashes.SHA512(),
-	length=32,  # AES-256 key length
-	salt=salt,
-	iterations=100000,
-	backend=default_backend()
-	)
-	return kdf.derive(password.encode())
+			# if no data is being received from the client, the connection will be closed
+			if not full_data:
+				return
+
+			print(f"[{datetime.now().time()}] Data received from {self.ip}:{self.port}")
+
+			# Step 2: Parses the salting, IV, and encrypted data
+			salt = full_data[:16] # 0 to 16 bytes is the salting
+			iv = full_data[16:32] # the IV is bytes 16 to 32
+			encrypted_data = full_data[32:] # the rest of the encrypted data is bytes 32 and onwards
+
+			# Step 3: Decrytion process
+			key = generate_key(password, salt)
+			decrypted_data = decrypt(encrypted_data, key, iv)
+
+			# Step 4: Saving the data to a file based off port
+			filename = f"received_file_{self.port}.txt"
+
+			with open(filename, 'wb') as f:
+				f.write(b"Encrypted Data:\n")
+				f.write(encrypted_data)
+				f.write(b"\n\nDecrypted Data:\n")
+			
+			print(f"File saved as '{filename}")
+
+		except Exception as e:
+			print(f"Connection error with {self.ip}:{self.port} - {e}")
+
+		finally:
+			self.client_socket.close()
+
+def server_start():
+	try:
+		server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		server_socket.bind((SERVER_IP, SERVER_PORT)) # binds the server ip and port to the socket
+
+		server_socket.listen(10) # can listen to 10 clients at the same time
+		print(f"Server is listening on {SERVER_IP}:{SERVER_PORT}...")
+
+		threads = []
+
+		while True:
+				(conn, (ip, port)) = server_socket.accept()
+
+				new_thread = ClientThread(ip, port, conn)
+
+				new_thread.start()
+				threads.append(new_thread)
+
+	except socket.error as e:
+		print(f"Error Creating the following socket: {e}")
+		sys.exit(1)
+
 
 def decrypt(ciphertext: bytes, key: bytes, iv: bytes) -> bytes:
 	cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
